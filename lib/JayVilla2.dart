673d0716e0895f6b5e2fbab2dba73d491195b430
Jay Villa <jovenjayvilla08@gmail.com>
2025-08-10 17:11:53 +0800

    feat(JayVill2): add JayVIlla2 with properties add method

---
+++ lib/JayVilla2.dart
@@ -0,0 +1,12 @@
+class  JayVilla2 {
+  String model;
+  int year;
+
+  // Constructor
+  JayVilla2({required this.model, required this.year});
+
+  // Method
+  void describe() {
+    print('Model: $model, Year: $year');
+  }
+}
