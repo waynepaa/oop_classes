d46cc0c4776803a60321f5fb1fa4b56c4b841c4b
waynepaa <jpaa.wayne@gmail.com>
2025-08-10 16:08:35 +0800

    feat(waynepaa1) : add waynepaa1 with properties and method

---
+++ lib/waynepaa1.dart
@@ -0,0 +1,12 @@
+class WaynePaa1 {
+  String model;
+  int year;
+
+  // Constructor
+  WaynePaa1({required this.model, required this.year});
+
+  // Method
+  void describe() {
+    print('Model: $model, Year: $year');
+  }
+}
